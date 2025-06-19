425dd69421c2f097dab2d56c24b695fe5564b53d
Andrew Taylor <rolyata47@gmail.com>
2025-06-19 12:56:30 -0500

    Adding comments

--- pi1.py
+++ pi1.py
@@ -1,3 +1,13 @@
+# We use the Taylor series for arctan(1) centered at a=0 to approximate Pi
+#
+# In other words, we use the equation Pi/4 = 1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 + ... to approximate Pi.
+#
+# We arrive at this equation by plugging x=1 into the Taylor series for arctan(x) centered at a=0
+#
+# This is an easy way to approximate Pi... it's hard to derive the equation, but easy to use the equation.
+#
+# We approximate Pi by generating a lower bound and an upper bound. Pi is somewhere between the two bounds.
+
 def approximate_pi(n):
     result = 0
     for i in range(0, n):
