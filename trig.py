6e9ce161d0994bc02115410c48b9c102af4c5b57
Andrew Taylor <rolyata47@gmail.com>
2025-06-19 17:20:41 -0500

    Adding cos(x) to trig.py

--- trig.py
+++ trig.py
@@ -49,5 +49,43 @@ def sin(x):
             ub -= term
     return (lb, ub)
 
+# To form the Taylor series for cos(x), we need to calculate the 1st, 2nd, 3rd derivatives at x=0
+#
+# f(x) = cos(x)
+# f'(x) = -sin(x)
+# f''(x) = -cos(x)
+# f'''(x) = sin(x)
+# f''''(x) = cos(x)
+#
+# f(0) = 1
+# f'(0) = 0
+# f''(0) = -1
+# f'''(0) = 0
+# f''''(0) = 1
+#
+# Now we can form a Taylor series for cos(x) centered at a=0.
+#
+# P(x) = f(0) + f'(0)*x + f''(0)*x^2/2! + f'''(0)*x^3/3! + ...
+# P(x) = 1 - x^2/2! + x^4/4! - x^6/6! + x^8/8! - x^10/10! + ...
+def cos(x):
+    lb = 0
+    ub = 0
+    n = 12
+    for i in range(0, n+3):
+        if i % 2 == 1:
+            continue
+        term = x**i / math.factorial(i)
+        if i % 4 == 0:
+            lb += term
+            ub += term
+        if i % 4 == 2:
+            lb -= term
+            if i < n+2:
+                ub -= term
+    return (lb, ub)
+
 bounds = sin(1)
 print("sin(1rad) is between %.15f and %.15f" %(bounds[0], bounds[1]))
+
+bounds = cos(1)
+print("cos(1rad) is between %.15f and %.15f" %(bounds[0], bounds[1]))
