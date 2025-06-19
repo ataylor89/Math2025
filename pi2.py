299c96e1ef80da9b37130c26319c319fd10145ba
Andrew Taylor <rolyata47@gmail.com>
2025-06-19 12:58:28 -0500

    Adding comments

--- pi2.py
+++ pi2.py
@@ -1,3 +1,12 @@
+# In this program, we approximate Pi using a Riemann sum.
+#
+# We approximate the area of the upper-right quadrant of the unit circle using a Riemann sum,
+# and then multiply that value by four, to get an approximation for Pi.
+#
+# We use a lower Riemann sum to get a lower bound, and an upper Riemann sum to get an upper bound.
+#
+# The value of Pi is somewhere between the lower bound and the upper bound.
+
 import math
 
 def approximate_pi(n):
