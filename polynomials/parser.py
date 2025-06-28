3944144e7bb6c52dc036c9777a469f1cee6753e3
Andrew Taylor <rolyata47@gmail.com>
2025-06-27 19:59:56 -0500

    Adding comments and explanation

--- polynomials/parser.py
+++ polynomials/parser.py
@@ -1,3 +1,18 @@
+# My family taught me that a parser breaks down a sentence into parts, or a file into parts
+#
+# Given the expression "3x^2 + 4x + 5", we are able to parse this expression, and break it down into parts
+#
+# First, the degree of the polynomial is 2. It is a quadratic polynomial, or a degree-2 polynomial.
+# Second, the coefficients are a=3, b=4, c=5.
+#
+# We store these coefficients in a map, where the key to each value is the degree of the term.
+# (e.g. coefficients[2] = 3, coefficients[1] = 4, coefficients[0] = 5)
+#
+# The parser contained in this file parses a polynomial expression, and breaks it down into parts,
+# so that we can easily access these parts while finding the roots of the polynomial.
+#
+# For instance, in order to apply the quadratic formula, we need to know a, b, and c.
+
 class ParseTree:
     def __init__(self, degree, coefficients):
         self.degree = degree
