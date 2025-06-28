e65acaa003250963d486049ab6adb0d7d1834407
Andrew Taylor <rolyata47@gmail.com>
2025-06-27 19:13:50 -0500

    Adding a quadratic.py module and a Quadratic class

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -1,5 +1,4 @@
-from parser import Parser, ParseTree
-import math
+from parser import Parser
 
 class ComplexNumber:
     def __init__(self, a, b):
@@ -15,29 +14,3 @@ class Polynomial:
         self.text = text
         self.degree = result.degree
         self.coefficients = result.coefficients
-
-    def solve(self):
-        if self.degree == 2:
-            return self._solve_quadratic()
-        else:
-            print("Currently, this class only supports quadratic polynomials.")
-
-    def _solve_quadratic(self):
-        if self.degree != 2:
-            return
-        a = self.coefficients[2]
-        b = self.coefficients[1]
-        c = self.coefficients[0]
-        if b**2 - 4*a*c >= 0:
-            root1 = ComplexNumber((-b + math.sqrt(b**2 - 4*a*c))/(2*a), 0)
-            root2 = ComplexNumber((-b - math.sqrt(b**2 - 4*a*c))/(2*a), 0)
-        else:
-            root1 = ComplexNumber(-b/(2*a), math.sqrt(-(b**2 - 4*a*c))/(2*a))
-            root2 = ComplexNumber(-b/(2*a), -1*math.sqrt(-(b**2 - 4*a*c))/(2*a))
-        return (root1, root2)
-
-text = "3x^2 + 4x + 5"
-p = Polynomial(text)
-solutions = p.solve()
-print("Polynomial: %s" %text)
-print("Roots: x = {%s, %s}" %(solutions[0], solutions[1]))
---
+++ polynomials/quadratic.py
@@ -0,0 +1,40 @@
+from polynomials import Polynomial, ComplexNumber
+import math
+import sys
+
+ERROR_MSG = 'Example usage: python quadratic.py "3x^2 + 4x + 5"'
+
+class Quadratic(Polynomial):
+    def __init__(self, text):
+        try: 
+            super().__init__(text)
+        except:
+            print(ERROR_MSG)
+            sys.exit(0)
+        if self.degree != 2:
+            print(ERROR_MSG)
+            sys.exit(0)
+
+    def solve(self):
+        a = self.coefficients[2]
+        b = self.coefficients[1]
+        c = self.coefficients[0]
+        if b**2 - 4*a*c >= 0:
+            root1 = ComplexNumber((-b + math.sqrt(b**2 - 4*a*c))/(2*a), 0)
+            root2 = ComplexNumber((-b - math.sqrt(b**2 - 4*a*c))/(2*a), 0)
+        else:
+            root1 = ComplexNumber(-b/(2*a), math.sqrt(-(b**2 - 4*a*c))/(2*a))
+            root2 = ComplexNumber(-b/(2*a), -1*math.sqrt(-(b**2 - 4*a*c))/(2*a))
+        return (root1, root2)
+
+def main():
+    if len(sys.argv) != 2:
+        print(ERROR_MSG)
+        sys.exit(0)
+
+    q = Quadratic(sys.argv[1])
+    solutions = q.solve()
+    print("x = {%s, %s}" %(solutions[0], solutions[1]))
+
+if __name__ == "__main__":
+    main()
