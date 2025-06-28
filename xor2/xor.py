a4f1fa8684913530b4bd68057c9d732551bf85b8
Andrew Taylor <rolyata47@gmail.com>
2025-06-28 14:28:33 -0500

    Switching order of variables

--- xor2/xor.py
+++ xor2/xor.py
@@ -1,13 +1,13 @@
 import sys
 
-ERROR_USER_INPUT = "Usage: python xor.py <keyfile> <msgfile>"
+ERROR_USER_INPUT = "Usage: python xor.py <msgfile> <keyfile>"
 ERROR_KEYSIZE = "The key has to be the same size as the message."
 
-def crypt(key, msg):
+def crypt(msg, key):
     filesize = len(msg)
 
-    keybytes = list(map(lambda x: ord(x), key))
     msgbytes = list(map(lambda x: ord(x), msg))
+    keybytes = list(map(lambda x: ord(x), key))
 
     out = [chr(msgbytes[i] ^ keybytes[i]) for i in range(0, filesize)]
     return ''.join(out)
@@ -16,18 +16,18 @@ def main():
     if len(sys.argv) != 3:
         print(ERROR_USER_INPUT)
         sys.exit(0)
-    
-    keyfile = open(sys.argv[1], "r")
-    key = keyfile.read()
 
-    msgfile = open(sys.argv[2], "r")
+    msgfile = open(sys.argv[1], "r")
     msg = msgfile.read()
 
-    if len(key) != len(msg):
+    keyfile = open(sys.argv[2], "r")
+    key = keyfile.read()
+
+    if len(msg) != len(key):
         print(ERROR_KEYSIZE)
         sys.exit(0)
 
-    print(crypt(key, msg), end='')
+    print(crypt(msg, key), end='')
     
 if __name__ == "__main__":
     main()
