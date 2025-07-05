dd8f8fc9154cbf97768e1fc7a00cfd42922de813
Andrew Taylor <rolyata47@gmail.com>
2025-07-05 14:02:08 -0500

    Adding new images and transformations

--- imagemagic/script.py
+++ imagemagic/script.py
@@ -2,11 +2,15 @@ from PIL import Image
 import imagemagic
 import math
 
+input_file = 'images/usflag/usflag.png'
+#output_file = 'images/usflag/transformations/reflecty.png'
+output_file = 'images/usflag/transformations/rotate90.png'
+
 #input_file = 'images/pawn/pawn.png'
 #output_file = 'images/pawn/transformations/rotate90.png'
 
-input_file = 'images/stacksofwheat/stacksofwheat.jpg'
-output_file = 'images/stacksofwheat/transformations/rotate90.jpg'
+#input_file = 'images/stacksofwheat/stacksofwheat.jpg'
+#output_file = 'images/stacksofwheat/transformations/rotate90.jpg'
 
 #input_file = 'images/beachatsainteadresse/beachatsainteadresse.jpg'
 #output_file = 'images/beachatsainteadresse/transformations/rotate90.jpg'
