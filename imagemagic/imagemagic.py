7d7693a83788da467dbf23e7c61356ec85eeb092
Andrew Taylor <rolyata47@gmail.com>
2025-07-05 12:33:58 -0500

    Adding coordinate transformation methods

--- imagemagic/imagemagic.py
+++ imagemagic/imagemagic.py
@@ -29,9 +29,7 @@ def transform(filename, A):
 
     X = (np.array((-w/2, h/2)), np.array((-w/2, -h/2)), np.array((w/2, h/2)), np.array((w/2, -h/2)))
     X = np.column_stack(X)
-
     Y = A @ X
-
     W = int(max(Y[0]) - min(Y[0]))
     H = int(max(Y[1]) - min(Y[1]))
 
@@ -40,20 +38,29 @@ def transform(filename, A):
     X = []
     for x in range(w):
         for y in range(h):
-            coordinates = (x + 0.5 - w/2, h/2 - (y + 0.5))
+            coordinates = matrix_to_cartesian(y, x, h, w)
             X.append(np.array(coordinates))
     X = np.column_stack(X)
-
     Y = A @ X
 
     newimg = Image.new(img.mode, (W, H), (0, 0, 0, 0))
     numpixels = W * H
 
     for i in range(numpixels):
-        x, y = int(X[0][i] + w/2), int(h/2 - X[1][i])
-        pixel = img.getpixel((x, y))
+        (row, col) = cartesian_to_matrix(X[0][i], X[1][i], w, h)
+        pixel = img.getpixel((col, row))
 
-        x, y = int(Y[0][i] + W/2), int(H/2 - int(Y[1][i]))
-        newimg.putpixel((x, y), pixel)
+        (row, col) = cartesian_to_matrix(Y[0][i], Y[1][i], W, H)
+        newimg.putpixel((col, row), pixel)
 
     return newimg
+
+def matrix_to_cartesian(i, j, n, m):
+    x = j + 0.5 - m/2
+    y = n/2 - (i + 0.5)
+    return (x, y)
+
+def cartesian_to_matrix(x, y, w, h):
+    i = int(h/2 - y)
+    j = int(w/2 + x)
+    return (i, j)
--- imagemagic/script.py
+++ imagemagic/script.py
@@ -2,11 +2,11 @@ from PIL import Image
 import imagemagic
 import math
 
-input_file = 'images/pawn/pawn.png'
-output_file = 'images/pawn/transformations/rotate90.png'
+#input_file = 'images/pawn/pawn.png'
+#output_file = 'images/pawn/transformations/rotate90.png'
 
-#input_file = 'images/stacksofwheat/stacksofwheat.jpg'
-#output_file = 'images/stacksofwheat/transformations/rotate90.jpg'
+input_file = 'images/stacksofwheat/stacksofwheat.jpg'
+output_file = 'images/stacksofwheat/transformations/rotate90.jpg'
 
 #input_file = 'images/beachatsainteadresse/beachatsainteadresse.jpg'
 #output_file = 'images/beachatsainteadresse/transformations/rotate90.jpg'
