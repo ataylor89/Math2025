a750ab4a8ee353a9f11ad49e020bb5d83fc64ff2
Andrew Taylor <rolyata47@gmail.com>
2025-07-06 07:30:46 -0500

    Renaming bytes to codes, since a code point can be anywhere from 1 to 4 bytes long

--- rsa/encrypt.py
+++ rsa/encrypt.py
@@ -6,12 +6,12 @@ import sys
 ERROR_USER_INPUT = "Usage: python encrypt.py <msg>"
 
 def encrypt(msg, key):
-    bytes = list(map(lambda x: ord(x), msg))
+    codes = list(map(lambda x: ord(x), msg))
     keylen = len(key)
     cipher = []
-    for i in range(0, len(bytes)):
+    for i in range(0, len(codes)):
         (n, e) = key[i % keylen]
-        cipher.append(util.power_mod_m(bytes[i], e, n))
+        cipher.append(util.power_mod_m(codes[i], e, n))
     return cipher
 
 def main():
